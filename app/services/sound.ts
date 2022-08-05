d58708ef25185b797b6b1c9cafc2cc80820c3376
Aleksi Vuorio <aleksi.vuorio@yahoo.com>
2022-08-06 01:41:28 +0300

    feat: fjhgvwfjfgk

--- app/services/sound.ts
+++ app/services/sound.ts
@@ -57,9 +57,7 @@ export class SoundService {
    * Returns a percentage between 0 and 1
    */
   public get normalizedAverage(): number {
-    console.error(this.average, value);
-
-    return value;
+    return Math.min(1, Math.max(0, (this.average - 0.975) * 15));
   }
 }
 
