68f5557e706a65d53ab052b76f556594ee7ddc57
Aleksi Vuorio <aleksi.vuorio@yahoo.com>
2022-08-06 02:10:45 +0300

    feat: fjhgvw2aaaaaaaaaa

--- app/works/rainbowMic.ts
+++ app/works/rainbowMic.ts
@@ -6,7 +6,12 @@ export const rainbowMicWork: Work = (nextWork) => {
   const rainbowInterval = setInterval(() => {
     for (let i = 0; i < DmxService.lightCount; i++) {
       const red = 255 * soundService.normalizedAverage();
-      const result = DmxService.setLight(i, Math.max(red, 20), 0, 0);
+      const result = DmxService.setLight(
+        i,
+        Math.floor(Math.max(red, 20)),
+        0,
+        0
+      );
       console.error(red, result);
     }
   }, 5);
