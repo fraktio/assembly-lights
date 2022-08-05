beb8b8094ab38d2b859384588343786122f5759e
Aleksi Vuorio <aleksi.vuorio@yahoo.com>
2022-08-05 15:53:27 +0300

    fix: blink colours

--- app/works/blink.ts
+++ app/works/blink.ts
@@ -4,19 +4,19 @@ import { DmxService } from "../services/dmx";
 export const blinkWork: Work = (nextWork) => {
   let tick = 0;
 
-  const White = [255, 255, 255];
-  const Dimm = [20, 20, 20];
+  const Red = [255, 0, 0];
+  const Blue = [0, 0, 255];
 
   const setLights = (): void => {
     for (let i = 0; i < DmxService.lightCount; i++) {
       const isEnabled = (tick + i) % 2 === 0;
 
       if (isEnabled) {
-        DmxService.setLight(i, White[0], White[1], White[2]);
+        DmxService.setLight(i, Red[0], Red[1], Red[2]);
         continue;
       }
 
-      DmxService.setLight(i, Dimm[0], Dimm[1], Dimm[2]);
+      DmxService.setLight(i, Blue[0], Blue[1], Blue[2]);
     }
     tick += 1;
 
