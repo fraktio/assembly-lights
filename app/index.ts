8f30215660a6eaddd467f3ab1f249887b5e131aa
mikkono <mikko.nousiainen@fraktio.fi>
2022-08-05 14:06:26 +0300

    Poor man's ritari assa.

--- app/index.ts
+++ app/index.ts
@@ -1,10 +1,15 @@
 import { DmxService } from "./services/dmx";
 
 let number = 0;
+let sequenceNumber = -1;
 
 setInterval(() => {
   number++;
-}, 500);
+  if (number === 256) {
+    number = 0;
+    sequenceNumber *= -1;
+  }
+}, 100);
 
 // input: h in [0,360] and s,v in [0,1] - output: r,g,b in [0,255]
 function hsv2rgb(h: number, s: number, v: number): [number, number, number] {
@@ -19,16 +24,37 @@ let tick = 0;
 setInterval(() => {
   const offset = 45 + (Math.sin(tick / 500) + 1) * 50;
 
-  for (let i = 0; i < DmxService.lightCount; i++) {
-    const color = hsv2rgb(tick + i * offset, 1, 1);
+  if (sequenceNumber === 1) {
+    for (let i = 0; i < DmxService.lightCount; i++) {
+      const color = hsv2rgb(tick + i * offset, 1, 1);
 
-    const bitMask = 1 << i;
-    const enabled = (number & bitMask) === bitMask;
-    if (enabled) {
-      DmxService.setLight(i, color[0], color[1], color[2]);
-      continue;
+      const bitMask = 1 << i;
+      const enabled = (number & bitMask) === bitMask;
+      if (enabled) {
+        DmxService.setLight(i, color[0], color[1], color[2]);
+        continue;
+      }
+      DmxService.setLight(i, 15, 15, 15);
     }
-    DmxService.setLight(i, 15, 15, 15);
+  }
+  if (sequenceNumber === -1) {
+    const color = [255, 10, 10];
+    for (let i = 0; i < DmxService.lightCount; i++) {
+      const index = number % (DmxService.lightCount * 2 - 2);
+      if (index < DmxService.lightCount) {
+        if (index === i) {
+          DmxService.setLight(i, color[0], color[1], color[2]);
+          continue;
+        }
+        DmxService.setLight(i, 15, 15, 15);
+      } else {
+        if (DmxService.lightCount * 2 - index - 2 === i) {
+          DmxService.setLight(i, color[0], color[1], color[2]);
+          continue;
+        }
+        DmxService.setLight(i, 15, 15, 15);
+      }
+    }
   }
   tick += 1;
 }, 5);
