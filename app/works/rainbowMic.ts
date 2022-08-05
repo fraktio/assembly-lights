af391fb263c9fda85be42c2f5cb8978e261257aa
Aleksi Vuorio <aleksi.vuorio@yahoo.com>
2022-08-06 01:50:14 +0300

    feat: fjhgvwfjfgkasd

--- app/works/rainbowMic.ts
+++ app/works/rainbowMic.ts
@@ -1,19 +1,11 @@
 import { Work } from "../queue";
-import { colorService } from "../services/color";
 import { DmxService } from "../services/dmx";
 import { soundService } from "../services/sound";
 
 export const rainbowMicWork: Work = (nextWork) => {
-  const startTime = Date.now();
-
   const rainbowInterval = setInterval(() => {
-    const time = Date.now() - startTime;
-    const soundRange = soundService.normalizedAverage;
-
     for (let i = 0; i < DmxService.lightCount; i++) {
-      const color = colorService.hsv2rgb(time / 10 + i * 10, soundRange, 1);
-
-      DmxService.setLight(i, color[0], color[1], color[2]);
+      DmxService.setLight(i, 255 * soundService.normalizedAverage, 0, 0);
     }
   }, 5);
 
