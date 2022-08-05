9be7bdd40fb2d477fa4dc9aae3ad558353c4fcc6
Petri Koivula <petri.koivula@fraktio.fi>
2022-08-05 16:30:48 +0300

    Use color service

--- app/works/rainbowSmooth.ts
+++ app/works/rainbowSmooth.ts
@@ -1,13 +1,6 @@
 import { Work } from "../queue";
+import { colorService } from "../services/color";
 import { DmxService } from "../services/dmx";
-
-// input: h in [0,360] and s,v in [0,1] - output: r,g,b in [0,255]
-function hsv2rgb(h: number, s: number, v: number): [number, number, number] {
-  const f = (n: number, k = (n + h / 60) % 6): number =>
-    Math.floor((v - v * s * Math.max(Math.min(k, 4 - k, 1), 0)) * 255);
-
-  return [f(5), f(3), f(1)];
-}
 
 export const rainbowSmoothWork: Work = (nextWork) => {
   let number = 0;
@@ -18,7 +11,7 @@ export const rainbowSmoothWork: Work = (nextWork) => {
     const time = Date.now() - startTime;
 
     for (let i = 0; i < DmxService.lightCount; i++) {
-      const color = hsv2rgb(time / 10 + i * 10, 1, 1);
+      const color = colorService.hsv2rgb(time / 10 + i * 10, 1, 1);
 
       DmxService.setLight(i, color[0], color[1], color[2]);
     }
